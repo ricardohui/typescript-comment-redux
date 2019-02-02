bc74dd923a3e4ca5c0e061b2a7356d0857c0069c
Ricardo <eehaha@gmail.com>
2019-02-02 09:45:01 +0800

    Refactor to extract Provider and store to Root

---
+++ src/Root.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+interface IParam {
+  children: React.ReactNode;
+  initialState?: {};
+}
+export default ({ children, initialState = {} }: IParam) => {
+  const middleware = [thunk];
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(...middleware)
+  );
+  return <Provider store={store}>{children}</Provider>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-// import * as reduxPromise from "redux-promise";
-import thunk from "redux-thunk";
 import App from "./App";
-import reducers from "./reducers";
-import registerServiceWorker from "./registerServiceWorker";
+import Root from "./Root";
 
-const middleware = [thunk];
-const store = createStore(reducers, applyMiddleware(...middleware));
 ReactDOM.render(
-  <Provider store={store}>
+  <Root>
     <App />
-  </Provider>,
+  </Root>,
   document.getElementById("root") as HTMLElement
 );
-registerServiceWorker();
